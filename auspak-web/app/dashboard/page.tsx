c7d082bcc040d155f9209a15d809cb0c83e0b3bb
podolskyDavid <david.podolskyi@tum.de>
2024-01-20 22:06:31 +0100

    dashboard

--- auspak-web/app/dashboard/page.tsx
+++ auspak-web/app/dashboard/page.tsx
@@ -13,6 +13,7 @@ import {GoogleMap, LoadScript} from '@react-google-maps/api';
 import Link from "next/link";
 import Metrics from "@/app/dashboard/components/metrics";
 import InteractiveMap from "@/app/dashboard/components/interactive-map";
+import Sidebar from "@/components/sidebard/sidebard";
 
 const googleMapsApiKey = process.env.PUBLIC_GOOGLE_MAPS_API_KEY;
 
@@ -31,58 +32,8 @@ export default function Dashboard() {
   return (
     <main>
       <div className="flex">
-        <div className="flex flex-col bg-auspak-green h-screen w-1/4 p-6">
-          <div id="logo-top" className="flex">
-            <Image
-              src="/auspak-name-logo.svg"
-              width={125}
-              height={125}
-              alt="auspak-name-logo"
-            />
-            <Badge className="ml-2 mt-1.5 mb-1.5 bg-auspak-white text-auspak-dark-grey">
-              beta
-            </Badge>
-          </div>
-          <div id="functionality-list" className="mt-[16.66vh]">
-
-            <div className="pb-6">
-              <div className="text-3xl font-bold">
-                John Doe
-              </div>
-              <div>
-                Regional Logistic Strategist
-              </div>
-            </div>
-            <div className="">
-              <Link
-                href=""
-                className="hover:underline"
-              >
-                button 1
-              </Link>
-              <br/>
-              <Link
-                href=""
-                className="hover:underline"
-              >
-                button 1
-              </Link>
-              <br/>
-              <Link
-                href=""
-                className="hover:underline"
-              >
-                button 1
-              </Link>
-
-            </div>
-
-          </div>
-          <div id="bottom-functionality" className="absolute bottom-6">
-            3
-          </div>
-        </div>
-        <div className="bg-auspak-white h-screen w-3/4 p-6">
+        <Sidebar/>
+        <div className="bg-auspak-white h-screen w-screen p-6">
           <Metrics/>
           <InteractiveMap/>
         </div>
