5a3f338d28af6e735b048d93e63cb71811a5cb31
Maxim Solodukhin <maxim.solodukhin@tum.de>
2024-01-22 04:10:55 +0100

    Adapted interative map to passed token and fetchData return type

--- auspak-web/app/dashboard/page.tsx
+++ auspak-web/app/dashboard/page.tsx
@@ -108,7 +108,7 @@ export default function Dashboard() {
           ) : (
             <BusDriverDashboard token={token!}/>
           )}
-          <InteractiveMap/>
+          <InteractiveMap token={token!}/>
         </div>
       </div>
     </main>
