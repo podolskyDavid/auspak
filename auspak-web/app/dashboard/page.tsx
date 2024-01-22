5118aa1d4a929fdc8783553f4177e09194e2a494
Maxim Solodukhin <maxim.solodukhin@tum.de>
2024-01-22 03:55:08 +0100

    token pass, empty sidebar on loading, always us fetch and sendData

--- auspak-web/app/dashboard/page.tsx
+++ auspak-web/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import Image from "next/image";
-import {Badge} from "@/components/ui/badge";
+import { Badge } from "@/components/ui/badge";
 import {
   Card,
   CardContent,
@@ -14,15 +16,98 @@ import Metrics from "@/app/dashboard/components/metrics";
 import InteractiveMap from "@/app/dashboard/components/interactive-map";
 import Sidebar from "@/components/sidebard/sidebard";
 import BusDriverDashboard from "@/app/dashboard/components/bus-driver-component";
+import { fetchData } from '../services/apiService';
+import React, { useEffect, useState } from 'react';
 
 export default function Dashboard() {
+  const [token, setToken] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true)
+  const [fullName, setFullName] = useState<string>('');
+  const [userEntity, setUserEntity] = useState<string>('');
+  
+  useEffect(() => {
+    const loadData = async (storedToken: string) => {
+      try {
+        if (storedToken) {
+          setIsLoading(true);
+          // Fetch data only when token is available
+          const meResponse = await fetchData('auth/me', { token: storedToken });
+          const meData = await meResponse.json();
+  
+          if (!meResponse.ok) {
+            console.error("Couldn't fetch personal data:", meData);
+            return;
+          }
+          if (!meData) {
+            // Redirect logic if data is not available
+            if (typeof window !== 'undefined') {
+              window.location.href = '/auth';
+            }
+            return;
+          }
+  
+          // Set full name and entity based on fetched data
+          setFullName(`${meData.first_name} ${meData.last_name}`);
+          setUserEntity(`${meData.entity}`);
+  
+          console.log('Data received:', meData);
+        }
+      } catch (error) {
+        console.error('Error fetching or processing data:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  
+    // Check if running in the browser environment
+    if (typeof window !== 'undefined') {
+      const storedToken = sessionStorage.getItem('access_token');
+      if (!token && storedToken) {
+        setToken(storedToken);
+        console.log('Token:', storedToken);
+        loadData(storedToken);
+      } else if (!storedToken) {
+        // Redirect logic if token is not set
+        window.location.href = '/auth';
+      }
+    }
+  }, [token]);
+
+  // // Render content conditionally based on loading state
+  if (isLoading) {
+    return (
+      <div className="flex flex-col bg-auspak-green h-screen w-1/4 max-w-80 min-w-64 p-6 justify-between">
+      <div>
+        <div id="logo-top" className="flex">
+          <Link href="/">
+              <Image
+                src="/auspak-name-logo.svg"
+                width={125}
+                height={125}
+                alt="auspak-name-logo"
+              />
+          </Link>
+          <Badge className="ml-2 mt-1.5 mb-1.5 bg-auspak-white text-auspak-dark-grey">
+            beta
+          </Badge>
+        </div>
+      </div>
+    </div>
+    )
+  }
+
   return (
     <main>
       <div className="flex">
-        <Sidebar/>
+        <Sidebar fullName={fullName} userEntity={userEntity.charAt(0).toUpperCase() + userEntity.slice(1)}/>
         <div className="flex flex-col bg-auspak-white h-screen w-screen p-6 gap-6">
-          <Metrics/>
-          <BusDriverDashboard/>
+          {userEntity === 'manager' ? (
+            <Metrics token={token!}/>
+          ) : userEntity === 'driver' ? (
+            <BusDriverDashboard token={token!}/>
+          ) : (
+            <BusDriverDashboard token={token!}/>
+          )}
           <InteractiveMap/>
         </div>
       </div>
